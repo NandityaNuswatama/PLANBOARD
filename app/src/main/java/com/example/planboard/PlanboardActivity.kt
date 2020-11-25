a4174770fd9e24ac7ee6f889c571df72353b13f6
NandityaNuswatama <66107893+NandityaNuswatama@users.noreply.github.com>
2020-11-25 14:17:03 +0700

    fixing bugs #11

--- app/src/main/java/com/example/planboard/PlanboardActivity.kt
+++ app/src/main/java/com/example/planboard/PlanboardActivity.kt
@@ -2,16 +2,12 @@ package com.example.planboard
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_planboard.*
-import me.ibrahimsn.lib.SmoothBottomBar
 
 class PlanboardActivity : AppCompatActivity() {
     private lateinit var navController: NavController
